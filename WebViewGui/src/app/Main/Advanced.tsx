cfc0ec573a165a8f177cb1238d6caab6d1c93971
joeunsoo <info@joeunsoo.com>
2025-09-09 14:42:30 +0900

    미사용 노브 제거

---
+++ WebViewGui/src/app/Main/Advanced.tsx
@@ -0,0 +1,38 @@
+import Stack from '@mui/material/Stack';
+
+import JuceKnob from '@/src/ui/Control/Slider/JuceKnob';
+
+export default function App() {
+  return (
+    <Stack
+      direction="row"
+      alignItems="center"
+      justifyContent="center"
+      sx={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(2, 1fr)'
+      }}
+    >
+      <JuceKnob
+        identifier="fastAttackSlider"
+        defaultValue={0.5}
+        subDigit={4}
+      />
+      <JuceKnob
+        identifier="fastReleaseSlider"
+        defaultValue={0.5}
+        subDigit={4}
+      />
+      <JuceKnob
+        identifier="slowAttackSlider"
+        defaultValue={0.5}
+        subDigit={4}
+      />
+      <JuceKnob
+        identifier="slowReleaseSlider"
+        defaultValue={0.5}
+        subDigit={4}
+      />
+    </Stack>
+  );
+}
