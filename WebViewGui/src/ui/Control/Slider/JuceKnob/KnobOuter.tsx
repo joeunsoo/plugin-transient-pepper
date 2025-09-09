d1592292522553e14cdb61607c0db75ed68ec23b
joeunsoo <info@joeunsoo.com>
2025-09-10 00:58:34 +0900

    시작 끝 값 dot 표시

--- WebViewGui/src/ui/Control/Slider/JuceKnob/KnobOuter.tsx
+++ WebViewGui/src/ui/Control/Slider/JuceKnob/KnobOuter.tsx
@@ -1,10 +1,27 @@
+function Dot({
+  ...props
+}:React.SVGProps<SVGCircleElement>) {
+
+  return (
+      <circle
+        r={4}
+        fill="var(--mui-palette-primary-main)"
+        {...props}
+      />
+  );
+}
+
 export default function Component() {
   const distance = 75;
+  const dotDistance = 76;
 
   const angle1 = (1 * 270) - 180;
   const radian1 = angle1 * Math.PI / 180;
   const x1 = distance * Math.cos(radian1);
   const y1 = distance * Math.sin(radian1);
+
+  const dotX = dotDistance * Math.cos(radian1);
+  const dotY = dotDistance * Math.sin(radian1);
 
   return (
     <g
@@ -22,6 +39,14 @@ export default function Component() {
         stroke="var(--mui-palette-primary-darkermost)"
         strokeWidth="4"
         style={{}}
+      />
+      <Dot
+        cx={-dotDistance}
+        cy={0}
+      />
+      <Dot
+        cx={dotX}
+        cy={dotY}
       />
     </g>
   );
