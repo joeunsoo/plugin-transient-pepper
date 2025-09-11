abe74bc9dd3b9b8df2191ce6dcaef1869e06bee5
joeunsoo <info@joeunsoo.com>
2025-09-12 00:49:45 +0900

    SectionTitle도 Bypass 반영

--- WebViewGui/src/app/Main/SectionTitle.tsx
+++ WebViewGui/src/app/Main/SectionTitle.tsx
@@ -1,14 +1,27 @@
+import { testOpacity } from '@/define';
+import { uesControlStore } from '@/store/ControlStore';
+import type { UIProps } from '@/types/UI';
 import Box, { type BoxProps } from '@mui/material/Box';
 
 export default function Component({
-  children, sx, ...props
-}: BoxProps) {
+  children,
+  sx,
+  ignoreBypass=false,
+  addTest=[],
+  ...props
+}: UIProps & BoxProps) {
+  const { bypassed } = uesControlStore();
+
   return (
     <Box
       sx={{
         fontWeight: 'var(--mui-fontWeight-lg)',
         fontSize: 'var(--mui-fontSize-sm)',
         color: 'var(--mui-palette-common-white)',
+        opacity: testOpacity([
+          bypassed && !ignoreBypass,
+          ...addTest
+        ]),
         ...sx,
       }}
       {...props}
