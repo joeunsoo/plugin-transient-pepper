0d2995bbccb59714c21cf743d4f972b42b7d2d41
joeunsoo <info@joeunsoo.com>
2025-09-15 10:17:22 +0900

    vit 빌드여부로 oncontextmenu

--- WebViewGui/src/app/Provider/IsDebug.tsx
+++ WebViewGui/src/app/Provider/IsDebug.tsx
@@ -1,24 +1,11 @@
 import Activate from '@/ui/Activate';
 
-import * as Juce from 'juce-framework-frontend';
-import { useEffect } from 'react';
-
-const isDebug = Juce.getNativeFunction('isDebug');
-
 export default function App() {
-  useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    isDebug().then((result: boolean) => {
-      if (!result) {
-        document.addEventListener('contextmenu', () => false);
-        document.addEventListener('selectstart', () => false);
-        document.addEventListener('dragstart', () => false);
-      }
-
-      return null;
-    }).catch(console.error);
-  }, []);
+  if (import.meta.env.PROD) {
+    window.addEventListener('contextmenu', (e) => e.preventDefault());
+    window.addEventListener('selectstart', (e) => e.preventDefault());
+    window.addEventListener('dragstart', (e) => e.preventDefault());
+  }
 
   return (
     <Activate />
