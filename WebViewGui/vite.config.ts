776886ddb0fede3d426211b0896b29f18487af74
joeunsoo <info@joeusnoo.com>
2025-09-19 02:58:54 +0900

    @vitejs/plugin-legacy

--- WebViewGui/vite.config.ts
+++ WebViewGui/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import legacy from '@vitejs/plugin-legacy';
 import svgr from 'vite-plugin-svgr';
 import checker from 'vite-plugin-checker';
 
 export default defineConfig({
   base: '/',
   server: {
-    open: true
+    open: true,
   },
   plugins: [
     react(),
@@ -21,12 +22,17 @@ export default defineConfig({
         },
       },
       overlay: {
-        initialIsOpen: 'error'
-      }
+        initialIsOpen: 'error',
+      },
+    }),
+    // Add the legacy plugin here
+    legacy({
+      targets: ['defaults', 'not IE 11', 'Safari >= 11'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'], // optional, for async/await support
     }),
   ],
   build: {
-    outDir: 'build'
+    outDir: 'build',
   },
   resolve: {
     alias: [
