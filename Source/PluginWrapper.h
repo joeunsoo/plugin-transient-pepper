1a6ea54950b2326e40a1874fe66a2552dd38dcd6
joeunsoo <info@joeunsoo.com>
2025-09-26 12:45:21 +0900

    웹뷰 c++ 영역 제거

--- Source/PluginWrapper.h
+++ Source/PluginWrapper.h
@@ -3,7 +3,6 @@
 #include <JuceHeader.h>
 #include "../Assets/DemoUtilities.h"
 #include "AudioProcessor/PluginAudioProcessor.h"
-#include "WebviewPluginEditor/WebviewPluginEditor.h"
 #include "PluginEditor/PluginEditor.h"
 
 class PluginAudioProcessorWrapper  : public PluginAudioProcessor
@@ -14,7 +13,6 @@ class PluginAudioProcessorWrapper  : public PluginAudioProcessor
   
   bool hasEditor() const override               { return true; }
   AudioProcessorEditor* createEditor() override {
-    // return new WebviewPluginEditor (*this);
     return new PluginEditor (*this);
   }
 };
